826f35cd0a1a4d1416ca5f8d426a50b6dccac7c4
pedro.frazao <apf-32@notafqdn>
2025-02-21 17:11:26 +0000

    asmObject with next_state attribute.

--- ppSlib/test_arena_sync_model.py
+++ ppSlib/test_arena_sync_model.py
@@ -33,5 +33,37 @@ class TestArenaSyncModel(unittest.TestCase):
         self.assertEqual( 1, self.arena.num_objects)
         self.__print_arena()
 
+
+class agentA(asmObject):
+    def run_interaction(self, context=None):
+        if( context is not None and int(context)>0 ):
+            s = self.get_obj_state()
+            s['e'] += 1
+            self.set_obj_state(s)
+        return super().run_interaction(context)
+
+class TestArenaSyncModelUpdate(unittest.TestCase):
+    def setUp(self):
+        self.arena = ArenaSyncModel(5, 5, sync_model='Sync')
+        obj1 = agentA("obj1", { 'e': 1, 'f': 0 }, 10)
+        obj2 = agentA("obj2", { 'e': 2, 'f': 0 }, 10)
+        self.arena.add_to_position(0, 0, obj1)
+        self.arena.add_to_position(0, 1, obj2)
+
+    def test_interaction(self):
+        self.assertEqual( 1, self.arena.get_position(0, 0)[0].state['e'] )
+        self.assertEqual( 2, self.arena.get_position(0, 1)[0].state['e'] )
+        o1 = self.arena.get_position(0, 0)[0]
+        o2 = self.arena.get_position(0, 1)[0]
+        o1.run_interaction( context=0 )
+        self.assertEqual( 1, o1.state['e'], "no change on the internal state" )
+        self.assertIsNone( o1._next_state, "no next state set" )
+        o1.run_interaction( context=1 )
+        self.assertEqual( 1, o1.state['e'], "no change on the internal state" )
+        self.assertIsNotNone( o1._next_state, "next state set" )
+        o2.run_update()
+        self.assertEqual( 2, o2.state['e'], "change on the internal state" )
+        
+
 if __name__ == '__main__':
-    unittest.main( verbosity=2 )
+    unittest.main( verbosity=4 )
